68cf5d94f32f1d0df0dc0948a3e1641f8ff4b886
Brandon-Datch <158134226+Brandon-Isaac@users.noreply.github.com>
2024-05-10 18:12:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { generateTextAndImage } from "./utils.js"
 let name = "Isaac Brandon Datch"
 let favoriteActivity = "hiking"
-let favoritePlace = "Sandy Beaches"
+let favoritePlace = "Wildlife"
 let temperature = 0.6
 generateTextAndImage(name, favoriteActivity, favoritePlace, temperature)
